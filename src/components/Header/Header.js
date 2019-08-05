5dec023b1e8d4c9ae3750fa8973f4ed159c4eeef
Dustin Haefele <Dustin.haefele@gmail.com>
2019-08-05 10:16:23 -0500

    styled login and registration forms

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -32,7 +32,7 @@ class Header extends Component {
     return (
       <nav>
         <Link to='/login'>Login</Link>
-        {' '}
+        {'  |  '}
         <Link to='/register'>Sign up</Link>
       </nav>
     )
--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -3,6 +3,7 @@ import { Input, Label } from '../Form/Form'
 import AuthApiService from '../../services/auth-api-service'
 import UserContext from '../../contexts/UserContext'
 import Button from '../Button/Button'
+import './LoginForm.css'
 
 class LoginForm extends Component {
   static defaultProps = {
@@ -56,6 +57,7 @@ class LoginForm extends Component {
           </Label>
           <Input
             ref={this.firstInput}
+            className='login-input'
             id='login-username-input'
             name='username'
             required
