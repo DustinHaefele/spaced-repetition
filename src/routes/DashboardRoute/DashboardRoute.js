07bc2d3b402d73177dd9d41fb7a2b576991a81ca
michaelbravo777 <michael.bravo777@gmail.com>
2019-08-06 11:49:51 -0400

    fixed rendering on login

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -4,16 +4,40 @@ import Button from '../../components/Button/Button';
 import WordList from '../../components/WordList/WordList';
 import MyLanguage from '../../components/MyLanguage/MyLanguage';
 import UserContext from '../../contexts/UserContext';
+import LanguageService from '../../services/language-service'
 
 class DashboardRoute extends Component {
+  state = {
+    language: null,
+    words: null
+  }
 
-  // static contextType = UserContext;
+  componentDidMount() {
+    this.fetchLanguage()
+  }
+
+  fetchLanguage = () => {
+    const state = { language: {}, words: [], error: null }
+
+    LanguageService.getLanguage().then( langPayload => {
+      if (langPayload) {
+        state.language = langPayload.language
+        state.words = langPayload.words
+      }
+      this.setState({ language: state.language })
+      this.setState({ words: state.words })
+      this.context.language = this.state.language
+      this.context.words = this.state.words
+    })
+  }
+
+  static contextType = UserContext;
   
   render() {
     
-    return (
-      <UserContext.Consumer>
-      {(value) => { console.log(value)
+    // return (
+      // <UserContext.Consumer>
+      // {(value) => { console.log(value)
         return (<section>
           <MyLanguage />
           <h3>Words to practice</h3>
@@ -23,10 +47,10 @@ class DashboardRoute extends Component {
               Start practicing
             </Button>
           </Link>
-          {this.context.language && (<p>Total correct answers: {this.context.language.total_score}</p>) }
-      </section>)}}
-      </UserContext.Consumer>
-    );
+          {this.state.language && (<p>Total correct answers: {this.state.language.total_score}</p>) }
+      </section>)
+      // </UserContext.Consumer>
+    // );
   }
 }
 
